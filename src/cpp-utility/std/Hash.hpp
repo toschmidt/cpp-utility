ee42ec16859407cb47e98f12141e487b629fb2e0
Tobias Schmidt <tobias.schmidt@in.tum.de>
2021-03-10 23:29:00 +0100

    implement the standard lib hash function for std::pair

---
+++ src/cpp-utility/std/Hash.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace std {
+   /// Partial specializations for pair types.
+   template<typename T1, typename T2>
+   struct hash<pair<T1, T2>> : public __hash_base<size_t, pair<T1, T2>> {
+      size_t operator()(pair<T1, T2> p) const noexcept {
+         return hash<T1>()(p.first) ^ hash<T2>()(p.second);
+      }
+   };
+} // namespace std
