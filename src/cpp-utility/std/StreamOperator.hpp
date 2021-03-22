48baeafdf01a724eb4ab22de7635f401538f2a5d
Tobias Schmidt <tobias.schmidt@in.tum.de>
2021-03-22 18:53:57 +0100

    Revert "fix multiple definitions of operator<<"
    This reverts commit 630acc84c5b0a89b72f14eda595cb17a3697d7d5.

--- src/cpp-utility/std/StreamOperator.hpp
+++ src/cpp-utility/std/StreamOperator.hpp
@@ -8,6 +8,12 @@ namespace std {
       return out;
    }
 
+   ostream& operator<<(ostream& out, const int8_t& v) {
+      // print 8-bit numbers as numbers and not as chars
+      out << static_cast<int>(v);
+      return out;
+   }
+
    template<typename T>
    ostream& operator<<(ostream& out, const vector<T>& v) {
       out << '[';
